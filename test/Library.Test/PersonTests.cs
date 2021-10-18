36d8f0d1f42ea073c17b62afa66f3a0a2bf4dcb5
Matias <MATIAS.CABRERA@correo.ucu.edu.uy>
2021-10-18 19:57:29 -0300

    Se agregaron los test

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -11,11 +11,41 @@ namespace Tests
         {
             // Insertá tu código de inicialización aquí
         }
+        
+        [Test]
+        public void TestNameValid() 
+        {
+            Person john = new Person("John Doe", "5.260.108-6");
+            string expected = "John Doe";
+            string actual = john.Name;
+            Assert.AreEqual(expected,actual);
+        }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestIdValid() 
         {
-            // Insertá tu código  de pruebaaquí
+            Person john = new Person("John Doe", "5.260.108-6");
+            string expected = "5.260.108-6";
+            string actual = john.ID;
+            Assert.AreEqual(expected,actual);
+        }
+        
+        [Test]
+        public void TestNameInvalid() 
+        {
+            Person jane = new Person("", "8.765.432-1");
+            string expected = null;
+            string actual = jane.Name;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void TestIdInvalid() 
+        {
+            Person jane = new Person("Jane Doe", "8.765.432-1");
+            string expected = null;
+            string actual = jane.ID;
+            Assert.AreEqual(expected,actual);
         }
     }
 }
